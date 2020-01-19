b962514fc40117da4efe8a2d43a5d90b32c51435
ahmdkhled <ahmdkhled@gmail.com>
2020-01-20 01:07:15 +0200

    make dialog cancellable only from later button

--- app/src/main/java/com/greyeg/tajr/view/dialogs/UpdateVersionDialog.java
+++ app/src/main/java/com/greyeg/tajr/view/dialogs/UpdateVersionDialog.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,15 +13,34 @@ import androidx.fragment.app.DialogFragment;
 
 import com.greyeg.tajr.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class UpdateVersionDialog extends DialogFragment {
 
+    @BindView(R.id.update)
+    Button update;
+    @BindView(R.id.later)
+    Button later;
+    @BindView(R.id.body)
+    TextView body;
+
+
+
     public UpdateVersionDialog() {
+
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View dialog=inflater.inflate(R.layout.update_version_dialog,container,false);
+        ButterKnife.bind(this,dialog);
+        setCancelable(false);
+
+        later.setOnClickListener(view -> {
+            this.dismiss();
+        });
 
         return dialog;
     }
