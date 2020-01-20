65a01ead414466143ee178e0cb8c687a43e65b6c
ahmdkhled <ahmdkhled@gmail.com>
2020-01-20 19:23:19 +0200

    fix:handle starting service in devices over android O

--- app/src/main/java/com/greyeg/tajr/over/MissedCallOrderService.java
+++ app/src/main/java/com/greyeg/tajr/over/MissedCallOrderService.java
@@ -1,8 +1,14 @@
 package com.greyeg.tajr.over;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import androidx.annotation.NonNull;
+
+import android.os.Build;
+import android.util.Log;
 import android.view.ContextThemeWrapper;
 
 import com.greyeg.tajr.R;
@@ -16,16 +22,32 @@ public class MissedCallOrderService extends HoverMenuService {
     private static final String TAG = "DemoHoverMenuService";
 
     public static void showFloatingMenu(Context context) {
-        context.startService(new Intent(context, MissedCallOrderService.class));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(new Intent(context, MissedCallOrderService.class));
+        }else
+            context.startService(new Intent(context, MissedCallOrderService.class));
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG, "onCreate: ");
+        Notification notification=new Notification();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel=new NotificationChannel("service","Service", NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+             notification = new Notification.Builder(getApplicationContext(),"service")
+                     .setContentTitle("service is running")
+             .build();
+        }
+        startForeground(11,notification);
     }
 
     @Override
     public void onDestroy() {
+        Log.d(TAG, "onCreate: ");
+
         super.onDestroy();
     }
 
